adff266e97ae91428eba3854ddc9ab35a199554a
Sezer Ökmen <szrokmn34@gmail.com>
2023-02-10 21:03:13 +0300

    header done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import "./App.scss";
+import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>Welcome To React</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem earum
-        incidunt recusandae facilis veniam excepturi nihil aspernatur deleniti a
-        aliquid?
-      </p>
+      <Header/>     
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+        <h1>Poplar Tour Places</h1>
+    </div>
+  )
+}
+
+export default Header
