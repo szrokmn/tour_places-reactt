418ee57e1d18f831728e7c67438cc57e3bec1147
Sezer Ökmen <szrokmn34@gmail.com>
2023-02-10 20:57:33 +0300

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.scss";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-    
+      <Navbar />
+      <h1>Welcome To React</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem earum
+        incidunt recusandae facilis veniam excepturi nihil aspernatur deleniti a
+        aliquid?
+      </p>
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <a href="#about">About</a>
+        <a href="#foryou">For You</a>
+        <a href="#services">Services</a>
+        <a href="#vlog">Vlog</a>
+        <a href="#contact">Contact</a>
+    </div>
+  )
+}
+
+export default Navbar
